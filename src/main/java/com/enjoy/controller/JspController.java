ac745be95718986e2d92658ae1dab2daf2e6f310
dkq6686 <Bob.Li@varian.com>
2019-09-26 13:59:09 +0800

    111

---
+++ src/main/java/com/enjoy/controller/JspController.java
@@ -0,0 +1,13 @@
+package com.enjoy.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+@RequestMapping("/jsp")
+public class JspController {
+    @RequestMapping("/hi")
+    public String sayHello() {
+        return "index";
+    }
+}
+
